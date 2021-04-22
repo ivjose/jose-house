d3ff88f9a5a569b11da8acec4264cebca3c1928c
ivjose <ivjosesantos@gmail.com>
2021-04-22 14:45:04 +0800

    Added search box api and component

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -4,7 +4,7 @@ import { useMutation, gql } from "@apollo/client";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -33,6 +33,8 @@ export default function HouseForm({}: IProps) {
     defaultValues: {},
   });
 
+  const address = watch("address");
+
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter you address" });
     register({ name: "latitude" }, { required: true, min: -90, max: 90 });
@@ -55,8 +57,16 @@ export default function HouseForm({}: IProps) {
         <label htmlFor="search" className="block">
           Search for your address
         </label>
-
+        <SearchBox
+          onSelectAddress={(address, latitude, longitude) => {
+            setValue("address", address);
+            setValue("latitude", latitude);
+            setValue("longitude", longitude);
+          }}
+          defaultValue=""
+        />
         {errors.address && <p>{errors.address.message}</p>}
+        <h2>{address}</h2>
       </div>
     </form>
   );
