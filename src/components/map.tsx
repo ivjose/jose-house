4fd9054cab546f955172b1ebd176b14009f51f86
ivjose <ivjosesantos@gmail.com>
2021-04-26 11:01:45 +0800

    Added search box list

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,7 +5,7 @@ import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery_houses } from "src/generated/HousesQuery";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 
 interface IProps {
   setDataBounds(bounds: string): void;
@@ -47,6 +47,26 @@ export default function Map({ setDataBounds, houses, highlightedId }: IProps) {
           }
         }}
       >
+        <div className="absolute top-0 z-10 w-full p-4">
+          <SearchBox
+            defaultValue=""
+            onSelectAddress={(_address, latitude, longitude) => {
+              if (longitude && latitude) {
+                setViewport((prevState) => ({
+                  ...prevState,
+                  longitude,
+                  latitude,
+                  zoom: 12,
+                }));
+
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          />
+        </div>
         {houses.map((house) => (
           <Marker
             key={house.id}
